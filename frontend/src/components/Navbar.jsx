c75bdadd3a272d5f9d2ac9d9532c881ed4ea8fec
ankit <saharan442090@gmail.com>
2025-04-04 16:04:21 +0530

    create Mobile Menu Toggle Button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,78 @@
+import { useState } from "react"
+
 const Navbar = () =>{
+  const [isOpen,setIsOpen] = useState(false)
   return(
+    <>
     <nav className="bg-blue-600 text-white flex justify-between items-center py-4 px-6">
         <h1 className="text-lg font-bold">TimeCue</h1>
+        
+{/* Mobile icon */}
+ <div className="md:hidden" aria-label="Toggle Menu" onClick={() => setIsOpen(!isOpen)}>
 
+   {isOpen ? (
+  // Close icon (X)
+  <svg
+    className="w-6 h-6 cursor-pointer"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>
+) : (
+  // Hamburger icon
+  <svg
+    className="w-6 h-6 cursor-pointer"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+  </svg>
+)}
+  </div>
+   
+  {/* desktop */}
       <ul className="hidden md:flex space-x-6 flex-1 justify-center">
         <li className="hover:text-blue-300 hover:cursor-pointer">Home</li>
         <li className="hover:text-blue-300 hover:cursor-pointer">Set Reminder</li>
         <li className="hover:text-blue-300 hover:cursor-pointer">My Reminders</li>
       </ul>
 
-     <div className="gap-4 flex">
+     <div className="gap-4 hidden md:flex">
       <button className="bg-white text-blue-600 px-4 py-2 rounded">Log In</button>
       <button className="bg-blue-500 px-4 py-2 rounded">Sign Up</button>
      </div>
     </nav>
+
+
+    {/* Mobile menu */}
+    {isOpen && (
+       <div className="md:hidden flex flex-col items-start space-y-4 mt-4 px-6 py-4 bg-blue-600 text-white shadow-md rounded transition-all duration-300">
+
+          <ul className="space-y-2">
+            <li className="hover:text-blue-300 cursor-pointer">Home</li>
+            <li className="hover:text-blue-300 cursor-pointer">Set Reminder</li>
+            <li className="hover:text-blue-300 cursor-pointer">My Reminders</li>
+          </ul>
+          <div className="flex flex-col gap-2 mt-4 ">
+            <button className="bg-white text-blue-600 px-4 py-2 rounded w-full">Log In</button>
+            <button className="bg-blue-500 px-4 py-2 rounded w-full">Sign Up</button>
+          </div>
+        </div>
+      )}
+      </>
   )
 }
 
 export default Navbar
+
+
