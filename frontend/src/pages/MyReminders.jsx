2147cb5c4ae36df9c0b0cf4e0ea3c5971f697a82
ankit <saharan442090@gmail.com>
2025-04-09 23:41:54 +0530

    delete req handle

--- frontend/src/pages/MyReminders.jsx
+++ frontend/src/pages/MyReminders.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import {getReminder} from "../services/Api"
+import {deleteReminder, getReminder} from "../services/Api"
 const MyReminders = () =>{
   const [reminders,setReminders] = useState([])
     const fetch = async () => {
@@ -10,10 +10,22 @@ const MyReminders = () =>{
         console.log(error.response?.data?.error);
       }  
     }
-  
     useEffect(()=>{
       fetch()
     },[])
+
+
+    const handleDelete = async(id) =>{
+      try {
+        const response = await deleteReminder(id);
+        alert(response.data.message);
+        const delete_Reminder = reminders.filter((reminder)=> reminder._id !== id)
+        setReminders(delete_Reminder)
+      } catch (error) {
+        
+      }
+    }
+
   return(
     <div className="max-w-3xl mx-auto p-4">
     <h2 className="text-2xl font-bold text-blue-700 mb-4">My Reminders</h2>
@@ -28,6 +40,10 @@ const MyReminders = () =>{
             <p className="text-gray-500 text-sm">
               {new Date(reminder.dateTime).toLocaleString()}
             </p>
+          <div className="flex gap-4 ">
+            <button className="" onClick={()=>handleDelete(reminder._id)}>delete</button>
+            <button>edit</button>
+          </div>
           </li>
         ))}
       </ul>
--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -2,11 +2,13 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const postReminder = async (reminderData) => {
-   console.log(reminderData);
   return await axios.post(`${API_URL}`,reminderData)
 }
 
 export const getReminder = async () => {
   return await axios.get(`${API_URL}`)
-  
+}
+
+export const deleteReminder = async (id) => {
+  return await axios.delete(`${API_URL}/${id}`)
 }
