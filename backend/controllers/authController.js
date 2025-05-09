cb12d2872529febdfaad7b09c74fd2f1fd385819
ankit <saharan442090@gmail.com>
2025-05-09 23:56:11 +0530

    create login controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const generateToken = require('../utils/generateToken')
 const sendTokenToCookie = require('../utils/sendTokenToCookie')
+const bcrypt = require('bcrypt')
 
 exports.signup = async (req,res) => {
   try {
@@ -27,6 +28,40 @@ exports.signup = async (req,res) => {
       }})
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
+
+exports.login = async (req,res) => {
+  try {
+    const {email,password} = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({message: 'Email and password required'}) 
+    }
+
+    const user = await User.findOne({email}).select('+password');
+    if(!user){
+      return res.status(401).json({message: 'Invalid credentials'})
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+    if (!isMatch) {
+      return res.status(401).json({message: 'Invalid credentials'})
+    }
+
+    const {accessToken,refreshToken} = generateToken(user._id)
+
+    sendTokenToCookie(res,accessToken,refreshToken)
+
+    const {_id,name}  = user;
+    res.status(200).json({
+      message: 'Login successful',
+      user: {_id,name, email}
+    })
+    
+  } catch (error) {
+     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 }
