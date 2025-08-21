b905d4e11aa3743b82bd0c454308e3adb0caf593
ankit <saharan442090@gmail.com>
2025-08-21 10:20:22 +0530

    fix: handle check-auth route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
-const {signup,login,logout,verifyEmail, refreshAccessToken, checkAuth, loginWithGoogle} = require('../controllers/authController')
+const {signup,login,logout,verifyEmail, refreshAccessToken,  loginWithGoogle} = require('../controllers/authController')
+const authMiddleware = require('../middlewares/authMiddleware')
  
 
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 router.post('/verify-email',verifyEmail)
-router.post("/refresh-token",refreshAccessToken)
-router.get("/check-auth",checkAuth)
+router.get("/refresh-token",refreshAccessToken)
+router.get("/check-auth",authMiddleware.protect,(req,res)=>{
+  const userId = req.user._id;
+  res.json({userId})
+})
 router.get("/google",loginWithGoogle)
 module.exports = router
