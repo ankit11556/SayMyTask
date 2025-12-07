08508d2565accc89a9b1fdab947096cf9f98f1d2
ankit <saharan442090@gmail.com>
2025-12-07 12:47:01 +0530

    feat: add edit user profile route and controller

--- backend/controllers/userProfileController.js
+++ backend/controllers/userProfileController.js
@@ -1,30 +1,51 @@
 const UserProfile = require("../models/UserProfile");
 
-exports.createUserProfile = async (req,res) => {
+exports.createUserProfile = async (req, res) => {
   try {
-     const {name,language} = req.body;   
-     const  userId = req.user._id
-   
-   const existing = await UserProfile.findOne({userId})
-   if(existing){
-    return res.status(400).json({message: "Profile aleardy exists"})
-   }
+    const { name, language } = req.body;
+    const userId = req.user._id;
 
-    const newProfile = await UserProfile.create({userId,name,language})
-    res.status(201).json({message:"Profile create successfully", newProfile: {
-    name: newProfile.name,
-    language: newProfile.language,
-  },})
+    const existing = await UserProfile.findOne({ userId });
+    if (existing) {
+      return res.status(400).json({ message: "Profile aleardy exists" });
+    }
+
+    const newProfile = await UserProfile.create({ userId, name, language });
+    res.status(201).json({
+      message: "Profile create successfully",
+      newProfile,
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Failed to create profile' });
+    res.status(500).json({ message: "Failed to create profile" });
   }
-}
+};
 
-exports.getUserProfile = async (req,res) => {
+exports.getUserProfile = async (req, res) => {
   try {
-    const profile = await UserProfile.find({userId: req.user._id})
-    res.status(200).json(profile) 
+    const profile = await UserProfile.find({ userId: req.user._id });
+    res.status(200).json(profile);
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
-}
+};
+
+exports.editUserProfile = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, language } = req.body;
+
+    const edit = await UserProfile.findByIdAndUpdate(
+      { _id: id, userId: req.user._id },
+      { name, language },
+      { new: true, runValidators: true }
+    );
+
+    if (!edit) {
+      return res.status(404).json({ error: "Profile not found" });
+    }
+
+    res.status(200).json({ message: "Profile updated successfully", edit });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/userProfileRoutes.js
+++ backend/routes/userProfileRoutes.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const router = express.Router()
-const {protect} = require('../middlewares/authMiddleware')
-const {createUserProfile,getUserProfile} = require('../controllers/userProfileController')
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
+const {
+  createUserProfile,
+  getUserProfile,
+  editUserProfile,
+} = require("../controllers/userProfileController");
 
-router.post("/",protect,createUserProfile)
-router.get("/",protect,getUserProfile)
-module.exports = router
+router.post("/", protect, createUserProfile);
+router.get("/", protect, getUserProfile);
+router.put("/:id", protect, editUserProfile);
+module.exports = router;
